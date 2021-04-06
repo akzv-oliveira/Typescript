b0376f96f6c25b4cedf2120c3215df1849258641
vinicius.s.oliveira <vinicius.s.olivera@accenture.com>
2021-04-06 11:56:37 -0300

    Adiciona a configuracao typescript do projeto

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    semi: true, // Integra as vírgulas nas linhas de código
+    trailingComma: 'all', // Insere uma vírgula pendente para duplicar códigos
+    singleQuote: true, // Habilita aspas simples ''
+    printWidth: 80, // Largura máxima da linha antes de quebrar a linha
+    tabWidth: 2,
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// MODULE MODE
+
+// export default nome;
+// ao exportar alguma coisa, muda-se o arquivo para module mode ao invés de script mode
+
+// ------------------------------------------------------------------------
